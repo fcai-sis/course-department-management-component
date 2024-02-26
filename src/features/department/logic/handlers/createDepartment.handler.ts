725c9ba6bc2eeb557cccab68327e2c5e543b25e4
Amr Khaled <lewolfje@gmail.com>
2024-02-26 21:56:04 +0200

    Add create department handler

---
+++ src/features/department/logic/handlers/createDepartment.handler.ts
@@ -0,0 +1,43 @@
+import mongoose from "mongoose";
+import { Request, Response } from "express";
+import DepartmentModel from "../../data/models/department.model";
+
+type HandlerRequest = Request<
+  {},
+  {},
+  {
+    code: string;
+    name: {
+      ar: string;
+      en: string;
+    };
+  }
+>;
+
+/**
+ * Create a department.
+ */
+
+const handler = async (req: HandlerRequest, res: Response) => {
+  const { code, name } = req.body;
+
+  const department = new DepartmentModel({
+    code,
+    name,
+  });
+
+  await department.save();
+
+  const response = {
+    message: "Department created successfully",
+    department: {
+      code: department.code,
+      name: department.name,
+    },
+  };
+
+  return res.status(201).json(response);
+};
+
+const createDepartmentHandler = handler;
+export default createDepartmentHandler;
