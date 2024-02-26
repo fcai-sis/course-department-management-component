b8743169b4a02d63f812fcc9d053f65cd0685cce
Amr Khaled <lewolfje@gmail.com>
2024-02-26 23:23:37 +0200

    Add updateDepartment handler

---
+++ src/features/department/logic/handlers/updateDepartment.handler.ts
@@ -0,0 +1,54 @@
+import { Request, Response } from "express";
+
+import DepartmentModel from "../../data/models/department.model";
+
+type HandlerRequest = Request<
+  {
+    departmentId: string;
+  },
+  {},
+  {
+    code?: string;
+    name?: {
+      ar: string;
+      en: string;
+    };
+  }
+>;
+
+/**
+ * Update a department by its ID.
+ */
+
+const handler = async (req: HandlerRequest, res: Response) => {
+  const departmentId = req.params.departmentId;
+
+  const department = await DepartmentModel.findByIdAndUpdate(
+    departmentId,
+    {
+      code: req.body.code,
+      name: req.body.name,
+    },
+    {
+      new: true,
+    }
+  );
+  if (!department) {
+    return res.status(404).json({
+      error: {
+        message: "Department not found",
+      },
+    });
+  }
+  const response = {
+    message: "Department updated successfully",
+    department: {
+      code: department.code,
+      name: department.name,
+    },
+  };
+  return res.status(200).json(response);
+};
+
+const updateDepartmentHandler = handler;
+export default updateDepartmentHandler;
