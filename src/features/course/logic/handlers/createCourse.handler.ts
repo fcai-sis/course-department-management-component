c35a659f1da6386306c75bd163ae861aab2397f9
Amr Khaled <lewolfje@gmail.com>
2024-02-26 21:55:31 +0200

    Add createCourse handler

---
+++ src/features/course/logic/handlers/createCourse.handler.ts
@@ -0,0 +1,54 @@
+import mongoose from "mongoose";
+import { Request, Response } from "express";
+import CourseModel from "../../data/models/course.model";
+
+type HandlerRequest = Request<
+  {},
+  {},
+  {
+    code: string;
+    name: {
+      ar: string;
+      en: string;
+    };
+    description: {
+      ar: string;
+      en: string;
+    };
+    department: mongoose.Types.ObjectId;
+    creditHours: number;
+  }
+>;
+
+/*
+ * Creates a course.
+ * */
+const handler = async (req: HandlerRequest, res: Response) => {
+  const { code, name, description, department, creditHours } = req.body;
+
+  const course = new CourseModel({
+    code,
+    name,
+    description,
+    department: department,
+    creditHours,
+  });
+
+  await course.save();
+
+  const response = {
+    message: "Course created successfully",
+    course: {
+      code: course.code,
+      name: course.name,
+      description: course.description,
+      department: course.department,
+      creditHours: course.creditHours,
+    },
+  };
+
+  return res.status(201).json(response);
+};
+
+const createCourseHandler = handler;
+export default createCourseHandler;
