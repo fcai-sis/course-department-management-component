3bbe6f2c4e57dedba0c04733b2587820ae08ea0a
Amr Khaled <lewolfje@gmail.com>
2024-02-26 23:23:31 +0200

    Add read  all departments  handler

---
+++ src/features/department/logic/handlers/readDepartments.handler.ts
@@ -0,0 +1,31 @@
+import { Request, Response } from "express";
+
+import DepartmentModel from "../../data/models/department.model";
+
+type HandlerRequest = Request;
+
+/**
+ * Reads all departments.
+ */
+
+const handler = async (req: HandlerRequest, res: Response) => {
+  // get the pagination parameters
+  const page = req.context.page;
+  const pageSize = req.context.pageSize;
+
+  const departments = await DepartmentModel.find()
+    .skip((page - 1) * pageSize)
+    .limit(pageSize);
+
+  const response = {
+    departments: departments.map((department) => ({
+      code: department.code,
+      name: department.name,
+    })),
+  };
+
+  return res.status(200).json(response);
+};
+
+const readDepartmentsHandler = handler;
+export default readDepartmentsHandler;
