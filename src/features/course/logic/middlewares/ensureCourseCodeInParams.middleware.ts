7044db4ed2050548bbcaa9b5d1fdece5772b1bc3
Amr Khaled <lewolfje@gmail.com>
2024-02-26 23:53:17 +0200

    add ensure course code in params middleware

---
+++ src/features/course/logic/middlewares/ensureCourseCodeInParams.middleware.ts
@@ -0,0 +1,41 @@
+import { Request, Response, NextFunction } from "express";
+import * as validator from "express-validator";
+
+const middlewares = [
+  validator
+    .param("courseCode")
+
+    .exists()
+    .withMessage("Course Code is required")
+
+    .custom((value) => {
+      // Course code must follow this pattern: 2-4 uppercase letters followed by 3 digits
+      const pattern = /^[A-Z]{2,4}\d{3}$/;
+      if (!pattern.test(value)) {
+        throw new Error(
+          "Invalid course code, must be 2-4 uppercase letters followed by 3 digits"
+        );
+      }
+
+      return true;
+    }),
+
+  (req: Request, res: Response, next: NextFunction) => {
+    const errors = validator.validationResult(req);
+
+    if (!errors.isEmpty()) {
+      return res.status(400).json({
+        error: {
+          message: errors.array()[0].msg,
+        },
+      });
+    }
+
+    req.params.courseCode = req.params.courseCode.trim();
+
+    next();
+  },
+];
+
+const ensureCourseCodeInParamsMiddleware = middlewares;
+export default ensureCourseCodeInParamsMiddleware;
